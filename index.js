fb12a4b013953a721c5433175dc76aab56f12ee7
Erik Zhao <erikzhao98@gmail.com>
2021-01-21 19:18:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-']+/g, '');
+  }
+
+  static titleize(string) {
+    let exceptions = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let results = []
+    let arrayString = string.split(' ')
+    for (let i = 0; i < arrayString.length; i++) {
+      if (exceptions.includes(arrayString[i])) {
+        results.push(arrayString[i])
+      } else {
+        results.push(this.capitalize(arrayString[i]))
+      }
+    }
+    return results.join(" ")
+  }
+  
+
 }
