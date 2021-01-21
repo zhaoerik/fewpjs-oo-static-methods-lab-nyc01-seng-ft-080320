fdf72a2894593c4d14ff1d551b41b78a4ac1e821
Erik Zhao <erikzhao98@gmail.com>
2021-01-21 19:25:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
-}
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string) {
+    let exceptions = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from', 'the']
+    let results = []
+    let arrayString = string.split(' ')
+    for (let i = 0; i < arrayString.length; i++) {
+      if (i === 0 || !exceptions.includes(arrayString[i])) {
+        results.push(this.capitalize(arrayString[i]))
+      } else {
+        results.push(arrayString[i])
+      }
+    }
+    return results.join(" ")
+    }
+  }
